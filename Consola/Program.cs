687543fd5067c91a233d76188352e55dc937f6d0
BrianB340452 <BB340452@fi365.ort.edu.uy>
2024-09-18 21:52:12 -0300

    Agrego sistema, ivalidable y unas funciones de menu

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("holala");
+﻿using Dominio;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sistema sistema = new Sistema();
+
+            MostrarMenu();
+            Console.WriteLine();
+
+        }
+
+        static void MostrarMenu()
+        {
+            MostrarMensajeColor(ConsoleColor.Cyan, "*** MENÚ ***");
+        }
+
+        static void MostrarMensajeColor(ConsoleColor color, string mensaje)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(mensaje);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static void MostrarError(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(mensaje);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
--- Dominio/Interfaces/IValidable.cs
+++ Dominio/Interfaces/IValidable.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dominio.Interfaces
+﻿namespace Dominio.Interfaces
 {
-    internal interface IValidable
+    public interface IValidable
     {
+        public void Validar();
     }
 }
