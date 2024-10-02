2028be94887d2e655ff2a5d159f76f625a3e0365
BrianB340452 <BB340452@fi365.ort.edu.uy>
2024-10-02 14:23:06 -0300

    Implementación de Enum EstadoPublicacion, y clases Oferta y Articulo.

--- Dominio/Sistema.cs
+++ Dominio/Sistema.cs
@@ -3,6 +3,8 @@
     public class Sistema
     {
         private List<Usuario> _listaUsuarios = new List<Usuario>();
+        private List<Articulo> _listaArticulos = new List<Articulo>();
+
         public Sistema() { }
     }
 }
